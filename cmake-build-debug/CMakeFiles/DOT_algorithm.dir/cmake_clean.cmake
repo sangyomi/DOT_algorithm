852b931380a00463a1a04b8d2554a8797b6bc521
sangyomi <kalvg7240@gmail.com>
2023-08-21 17:58:28 +0900

    1

---
+++ cmake-build-debug/CMakeFiles/DOT_algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DOT_algorithm.dir/main.cpp.o"
+  "CMakeFiles/DOT_algorithm.dir/main.cpp.o.d"
+  "DOT_algorithm"
+  "DOT_algorithm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DOT_algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
